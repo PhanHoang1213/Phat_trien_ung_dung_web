06e5208963469b721c6a1d310bc664aee4c3cc5a
Phanhoang1213 <hoang061213@gmail.com>
2023-11-07 19:14:40 +0700

    slide 25

---
+++ PHP/slide25.php
@@ -0,0 +1,10 @@
+<?php
+$Subject = "Lập trình ứng dựng web";
+$universityName = "HUMG";
+$firstNumber = 2;
+$secondNumber = 2;
+echo "Học " . $Subject . "ở phòng 502 tại trường " . $universityName . "<br>";
+echo "Học $Subject ở phòng 502 tại trường $universityName <br>";
+echo "<h1>$universityName</h1>";
+echo $firstNumber + $secondNumber;
+?>
