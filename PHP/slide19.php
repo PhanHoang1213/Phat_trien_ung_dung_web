bb15193d06089d131ecb846faa14075747db6385
Phanhoang1213 <hoang061213@gmail.com>
2023-11-07 19:03:34 +0700

    slide 17

---
+++ PHP/slide19.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "MY FIRST PHP FILE";
+    ?>
+    <h3>Nguyễn Văn Hoàng</h3>
+    <select name="" id="">
+        <option value="">1</option>
+        <option value="">2</option>
+    </select>
+</body>
+
+</html>
