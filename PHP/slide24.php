ae383472e6a0d3027be7c245cd3fa65370883690
Phanhoang1213 <hoang061213@gmail.com>
2023-11-07 19:10:13 +0700

    slide 24

---
+++ PHP/slide24.php
@@ -0,0 +1,8 @@
+<?php
+$hoangAccount = 10000000;
+echo "So tien trong tai khoan Hoang: $hoangAccount ";
+$humgPay = 10000000;
+echo "HUMG chuyen tien: $humgPay";
+$hoangAccount = $hoangAccount + $humgPay;
+echo "So tien trong tai khoan Hoang: $hoangAccount";
+?>
