3b5e72d68277c36f35829a4ef7f5140a86eda7c3
Phanhoang1213 <hoang061213@gmail.com>
2023-11-08 14:58:56 +0700

    slide 30

---
+++ PHP/slide30.php
@@ -0,0 +1,19 @@
+<?php
+// Thực hành 1
+$firstNumber;
+$secondNumber;
+// Thực hành 2
+$a;
+$b;
+$c;
+$delta = b^2 -4*a*c;
+$x1; $x2;
+// Thực hành 3
+$chuCanTimKiem;
+$ChuoiTimkiem;
+$soLanXuatHien;
+// Thực hành 4
+$chieuCao;
+$chieuRong;
+$loaiTuGiac;
+?>
