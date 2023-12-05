c8063d9fe4cb3f42f14cc5966a873fcd39d094c4
Phanhoang1213 <hoang061213@gmail.com>
2023-12-05 23:36:02 +0700

    challenge 1

--- PHP/MySQL/connect.php
+++ PHP/MySQL/connect.php
@@ -8,4 +8,3 @@ $conn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "<h4 style='text-align:center;'>Connected successfully</h4>";
