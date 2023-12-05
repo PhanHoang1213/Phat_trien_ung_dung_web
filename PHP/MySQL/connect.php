72af2434d84a8a74fec470fcdbd64e3329a4fdf4
Phanhoang1213 <hoang061213@gmail.com>
2023-12-05 22:33:15 +0700

    Thực hành 6

--- PHP/MySQL/connect.php
+++ PHP/MySQL/connect.php
@@ -8,4 +8,4 @@ $conn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully <br>";
+echo "<h4 style='text-align:center;'>Connected successfully</h4>";
