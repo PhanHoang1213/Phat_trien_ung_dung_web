d755c8ef957bde22f6d829624c5762ff1f2f6db3
Phanhoang1213 <hoang061213@gmail.com>
2023-12-15 11:31:08 +0700

    User Home

---
+++ PHP/login/userHome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    if (isset($_SESSION['userId'])) {
+        echo 'Bạn đã đăng ký với tên là ' . $_SESSION['userId'] . "<br>";
+        echo "Click vào đây để <a href='userLogout.php'>Logout</a>";
+    }
+    ?>
+    <ul>
+        <li>
+            <div>MENU</div>
+        </li>
+        <li><a href="./loginForm.php">Đăng nhập</a></li>
+        <li><a href="">Xem chuyến bay</a></li>
+        <li><a href="">Mua vé</a></li>
+        <li><a href="">Thêm chuyến bay</a></li>
+    </ul>
+</body>
+
+</html>
